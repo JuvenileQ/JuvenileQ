35874043dd13cb27652503bb1e6b8684daa5f57d
qinyj <qinyj@deepblueai.com>
2023-05-04 11:13:06 +0800

    🐛 fix: 修改页面

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ function generateProgressBar() {
     const progressBarCapacity = 30
     const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
     const progressBar =
-      '█'.repeat(passedProgressBarIndex) +
-      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
+        '█'.repeat(passedProgressBarIndex) +
+        '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
     return `{ ${progressBar} }`
 }
 
 const readme = `\
-<div>⏳ Year progress <span>${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %</span></div>
-<h1 align="center"><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30" alt='Hello'/> Hey! 👋  Nice to see you💬✨.</h1>
-
-
-
----
-<p align="center">⏰该 <b>README</b> 文件 <b>每三小时</b> 更新一次！（坐标济南）🌱 上次更新于: **${new Date().toLocaleDateString().replace(/\//g, "-") + " " + new Date().toTimeString().substr(0, 8)}**<br />
-
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)
-<img alt="Stars" src="https://img.shields.io/github/stars/thmsgbrt/thmsgbrt?style=flat-square&labelColor=343b41"/> 
-<img alt="Forks" src="https://img.shields.io/github/forks/thmsgbrt/thmsgbrt?style=flat-square&labelColor=343b41"/>
+<div>
+    <div style={'display':'flex','justify-content':'space-between'}>
+        <span style={'font-size':'12px'}>⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %</span>
+        <div>![GitHub Actions](https://github.com/JuvenileQ/JuvenileQ/workflows/profile/badge.svg)</div>
+    </div>
+    <br/>
+    <h1 align="center"><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30" alt='Hello'/> Hey! 👋  Nice to see you💬✨.</h1>
+    ---
+    <p align="center">⏰该 <b>README</b> 文件 <b>每三小时</b> 更新一次！（坐标济南）🌱 上次更新于: ** ${new Date().toLocaleDateString('zh-CN', { timeZone: 'Asia/Shanghai' }).replace(/\//g, "-") + " " + new Date().toTimeString().substr(0, 8)} **<br />
+</div>
 \
 `
 

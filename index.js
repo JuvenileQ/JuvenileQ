0c6c3a7604a2d47b38d319dc374da0eb92b129c1
qinyj <qinyj@deepblueai.com>
2023-05-25 17:19:41 +0800

    🐛 fix: 修改页面

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ function generateProgressBar() {
 }
 
 const readme = `\
-<div>
-    <div style={'display':'flex','justify-content':'space-between'}>
-        <span style={'font-size':'12px'}>⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %</span>
-        <div>![GitHub Actions](https://github.com/JuvenileQ/JuvenileQ/workflows/profile/badge.svg)</div>
-    </div>
-    <br/>
-    <h1 align="center"><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30" alt='Hello'/> Hey! 👋  Nice to see you💬✨.</h1>
-    ---
-    <p align="center">⏰该 <b>README</b> 文件 <b>每三小时</b> 更新一次！（坐标济南）🌱 上次更新于: ** ${new Date().toLocaleDateString('zh-CN', { timeZone: 'Asia/Shanghai' }).replace(/\//g, "-") + " " + new Date().toTimeString().substr(0, 8)} **<br />
-</div>
-\
-`
+[comment]: <> (⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %)
 
-console.log(readme)
+<p align="center">
+    
+</p>
+
+<br/>
+
+<h1 align="center"><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30" alt='Hello'/> Hey! 👋  Nice to see you💬✨.</h1>
+<br/>
+
+
+<br/>
+<p align="center">
+    ⏰上述信息 <b>每12小时</b> 更新一次！（坐标济南）🌱 上次更新于: <b>${new Date().toLocaleDateString('zh-CN', {timeZone: 'Asia/Shanghai'})</b>![GitHub Actions](https://github.com/JuvenileQ/JuvenileQ/workflows/profile/badge.svg)
+</p>
+\`

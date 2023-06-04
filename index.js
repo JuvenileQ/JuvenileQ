ffa4e8aabc04af65090b586b61c8c6cb14cecfd2
qinyj <qinyj@deepblueai.com>
2023-06-04 21:38:17 +0800

    🐛 debug: 调试

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const readme = `\
 <h1 align="center"><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30" alt='Hello'/>Hey! Nice to see you.👋</h1>
 <p align="center">
-    <a href="https://github.com/JuvenileQ">
+    <a target="_blank" href="https://github.com/JuvenileQ">
         <img alt="GitHub" src="https://img.shields.io/badge/GitHub-JuvenileQ-d7d7d7?logo=github" />
     </a>
-    <a href="https://juejin.cn/user/4187374087573293">
+    <a target="_blank" href="https://juejin.cn/user/4187374087573293">
         <img alt="掘金" src="https://img.shields.io/badge/掘 金-Juvenile丶-2689f3?logo=juejin" />
     </a>
-    <a href="https://blog.csdn.net/qq_37424916">
+    <a target="_blank" href="https://blog.csdn.net/qq_37424916">
         <img alt="CSDN" src="https://img.shields.io/badge/CSDN-秦 延 金-fc5531?logo=csdn&logoColor=fff" />
     </a>
-    <a href="https://space.bilibili.com/303638383">
+    <a target="_blank" href="https://space.bilibili.com/303638383">
         <img alt="哔哩哔哩" src="https://img.shields.io/badge/哔哩哔哩-Juvenile丶Q-fa719a?logo=bilibili&logoColor=fff" />
     </a>
-    <a href="https://www.zhihu.com/people/qinyanjin">
+    <a target="_blank" href="https://www.zhihu.com/people/qinyanjin">
         <img alt="知乎" src="https://img.shields.io/badge/知乎-秦 始 皇-2689f3?logo=zhihu&logoColor=fff" />
     </a>
-    <a href="https://gitee.com/qinyanjin">
+    <a target="_blank" href="https://gitee.com/qinyanjin">
         <img alt="Gitee" src="https://img.shields.io/badge/Gitee-秦 延 金-c51a29?logo=gitee&logoColor=fff" />
     </a>
-    <a href="https://www.yuque.com/qinyanjin">
+    <a target="_blank" href="https://www.yuque.com/qinyanjin">
         <img alt="语雀" src="https://img.shields.io/badge/语 雀-Juvenile丶🐳-4fc36d?logo=yuque&logoColor=fff" />
     </a>
 </p>
-<a href="https://github.com/JuvenileQ">
+
+
+<a target="_blank" href="https://github.com/JuvenileQ">
     <div align="right" >
         <img align="right" src="https://access-counter-a25rw2wtg-qinyanjin.vercel.app/api/counter?name=JuvenileQ&theme=001&length=8" alt="qinyanjin" />
     </div>
 </a>
 
-#### About Me 👋
+
+#### About 👋
 
 - ✨ From **Jinan, Shandong, China**
 - 🌵 I am a full stack **Developer**
@@ -37,20 +40,23 @@ const readme = `\
 - 🌈 **Study** with me
 - 🌱 Follow me on [GitHub](https://github.com/JuvenileQ) ,[掘金](https://juejin.cn/user/4187374087573293),[CSDN](https://blog.csdn.net/qq_37424916),[哔哩哔哩](https://space.bilibili.com/303638383),[Gitee](https://gitee.com/qinyanjin) or [知乎](https://www.zhihu.com/people/qinyanjin)
 
+
 #### Weekly development breakdown 📊
 
 <!--START_SECTION:Qin-->
 <!--END_SECTION:Qin-->
 
+
 <p>
-    <a href="https://github.com/JuvenileQ">
+    <a target="_blank" href="https://github.com/JuvenileQ">
         <img width=410px alt='qinyanjin' src="https://github-readme-stats-theta-murex.vercel.app/api?username=JuvenileQ&hide_title=true&locale=cn&hide_rank=true&show_icons=true&include_all_commits=true&count_private=true"/>
     </a>
-     <a href="https://github.com/JuvenileQ">
+     <a target="_blank" href="https://github.com/JuvenileQ">
         <img width=410px alt='qinyanjin' src="https://github-readme-stats-theta-murex.vercel.app/api/top-langs?username=JuvenileQ&layout=compact&hide_title=true&langs_count=8&locale=cn&include_all_commits=true&count_private=true"/>
     </a>
 </p>
 
+---
 
 <p align="center">
     ⏰每12小时定时更新，上次更新于：${new Date().toLocaleDateString('zh-CN', {timeZone: 'Asia/Shanghai'})}
